7c3fe652f7ce68bea98fc36ac7aaa972d642c7e5
francozanini <francozanini1995@gmail.com>
2022-05-25 18:53:07 -0300

    added more pc constructors

---
+++ armory-server/src/main/kotlin/io/fz/armory/domain/Attributes.kt
@@ -0,0 +1,3 @@
+package io.fz.armory.domain
+
+data class Attributes(val str: Int, val dex: Int, val con: Int, val int: Int, val wis: Int, val cha: Int)
--- armory-server/src/main/kotlin/io/fz/armory/domain/PlayableCharacter.kt
+++ armory-server/src/main/kotlin/io/fz/armory/domain/PlayableCharacter.kt
@@ -2,15 +2,36 @@ package io.fz.armory.domain
 
 class PlayableCharacter private constructor(
     private val name: String,
+    val race: Race,
+    val playableClass: ClassName,
     strength: Int,
     dexterity: Int,
     constitution: Int,
     intelligence: Int,
     wisdom: Int,
-    charisma: Int,
-    val playableClass: ClassName,
-    val race: Race
+    charisma: Int
 ) {
+
+    constructor(
+        name: String,
+        strength: Int,
+        dexterity: Int,
+        constitution: Int,
+        intelligence: Int,
+        wisdom: Int,
+        charisma: Int,
+        playableClass: ClassName, raceName: RaceName
+    ) : this(
+        name,
+        Race.named(raceName),
+        playableClass,
+        strength,
+        dexterity,
+        constitution,
+        intelligence,
+        wisdom,
+        charisma
+    )
 
     private val strength: Attribute
     private val dexterity: Attribute
@@ -75,6 +96,19 @@ class PlayableCharacter private constructor(
 
     companion object CreatureCompanion {
         const val NAME_CAN_NOT_BE_EMPTY = "Name can not be empty"
+
+        fun named(name: String, raceName: RaceName, className: ClassName, attributes: Attributes) = PlayableCharacter(
+            name,
+            attributes.str,
+            attributes.dex,
+            attributes.con,
+            attributes.int,
+            attributes.wis,
+            attributes.cha,
+            className,
+            raceName
+        )
+
         fun named(
             name: String,
             strength: Int,
@@ -85,14 +119,14 @@ class PlayableCharacter private constructor(
             charisma: Int
         ) = PlayableCharacter(
             name,
+            Race.HUMAN,
+            ClassName.FIGHTER,
             strength,
             dexterity,
             constitution,
             intelligence,
             wisdom,
-            charisma,
-            ClassName.FIGHTER,
-            Race.HUMAN
+            charisma
         )
     }
 
