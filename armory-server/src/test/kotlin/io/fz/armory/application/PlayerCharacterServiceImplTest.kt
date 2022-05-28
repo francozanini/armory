fed96a36928ccd3db5cb986b0693d523ef2c0b3c
francozanini <francozanini1995@gmail.com>
2022-05-28 18:00:56 -0300

    moved PlayerCharacterServiceImplTest.kt to applciartion package

---
+++ armory-server/src/test/kotlin/io/fz/armory/application/PlayerCharacterServiceImplTest.kt
@@ -0,0 +1,41 @@
+package io.fz.armory.application
+
+import io.fz.armory.adapter.out.memory.InMemoryReactivePlayerCharacterRepository
+import io.fz.armory.domain.Attributes
+import io.fz.armory.domain.ClassName
+import io.fz.armory.domain.RaceName
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import reactor.test.StepVerifier
+
+internal class PlayerCharacterServiceImplTest {
+
+    private lateinit var reactivePlayerCharacterRepository: ReactivePlayerCharacterRepository
+    private lateinit var playerCharacterServiceImpl: PlayerCharacterServiceImpl
+
+    @BeforeEach
+    fun setUp() {
+        reactivePlayerCharacterRepository = InMemoryReactivePlayerCharacterRepository()
+        playerCharacterServiceImpl = PlayerCharacterServiceImpl(reactivePlayerCharacterRepository)
+    }
+
+    @Test
+    fun canCreate() {
+        val bargusMono =
+            playerCharacterServiceImpl.create(
+                "Bargus",
+                RaceName.DWARF,
+                ClassName.CLERIC,
+                Attributes(15, 10, 14, 8, 13, 12)
+            )
+
+        StepVerifier
+            .create(bargusMono)
+            .consumeNextWith { bargus ->
+                assertThat(bargus.mongoId).isNotNull
+                assertThat(reactivePlayerCharacterRepository.findById(bargus.mongoId!!).block()).isPresent
+            }
+            .verifyComplete()
+    }
+}
