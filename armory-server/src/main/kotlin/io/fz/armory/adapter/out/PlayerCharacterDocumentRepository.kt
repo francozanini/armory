be599c0542e1f08b2a041bab614a1f656d3f0f5b
francozanini <francozanini1995@gmail.com>
2022-05-28 00:46:51 -0300

    pcdocumentrepository uses string for ids

--- armory-server/src/main/kotlin/io/fz/armory/adapter/out/PlayerCharacterDocumentRepository.kt
+++ armory-server/src/main/kotlin/io/fz/armory/adapter/out/PlayerCharacterDocumentRepository.kt
@@ -4,4 +4,4 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface PlayerCharacterDocumentRepository : ReactiveMongoRepository<PlayerCharacterDocument, Long>
+interface PlayerCharacterDocumentRepository : ReactiveMongoRepository<PlayerCharacterDocument, String>
