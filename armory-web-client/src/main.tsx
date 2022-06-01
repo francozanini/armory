abc44751d816048f5ff9f8e3ee9c494f120f7581
francozanini <francozanini1995@gmail.com>
2022-05-31 21:36:20 -0300

    fonts

--- armory-web-client/src/main.tsx
+++ armory-web-client/src/main.tsx
@@ -1,12 +1,21 @@
-import {ChakraProvider} from '@chakra-ui/react'
+import {ChakraProvider, extendTheme} from '@chakra-ui/react'
+import '@fontsource/raleway/400.css'
+import '@fontsource/open-sans/700.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+const theme = extendTheme({
+    fonts: {
+        heading: `'Open Sans', sans-serif`,
+        body: `'Raleway', sans-serif`,
+    },
+})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
             <App/>
         </ChakraProvider>
     </React.StrictMode>,
