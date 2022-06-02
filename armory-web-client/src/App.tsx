2fa8f93606fd1caa6ed13204f30aa250df6a49be
francozanini <francozanini1995@gmail.com>
2022-06-02 19:38:49 -0300

    tabs

--- armory-web-client/src/App.tsx
+++ armory-web-client/src/App.tsx
@@ -7,8 +7,13 @@ import {
     FormLabel,
     Input,
     Select,
+    Tab,
     Table,
     TableContainer,
+    TabList,
+    TabPanel,
+    TabPanels,
+    Tabs,
     Tbody,
     Td,
     Th,
@@ -104,31 +109,47 @@ const Abilities = () => {
 
 function App() {
     return (
-        <Container>
-            <Box background={"blackAlpha.600"} m={[2, 3]} p={[1, 2]} borderRadius='lg' shadow={"xl"}>
-                <Flex m={[2, 3]} gap={5} flexDirection={"column"}>
-                    <FormControl>
-                        <FormLabel>Name</FormLabel>
-                        <Input placeholder=' '/>
-                    </FormControl>
-                    <FormControl>
-                        <FormLabel>Class</FormLabel>
-                        <Select placeholder=' '>
-                            <option value='fighter'>Fighter</option>
-                            <option value='cleric'>Cleric</option>
-                        </Select>
-                    </FormControl>
-                    <FormControl>
-                        <FormLabel>Race</FormLabel>
-                        <Select placeholder=' '>
-                            <option value='human'>Human</option>
-                            <option value='dwarf'>Dwarf</option>
-                        </Select>
-                    </FormControl>
-                    <Abilities/>
-                </Flex>
-            </Box>
-        </Container>)
+        <Tabs>
+            <TabList>
+                <Tab>Details</Tab>
+                <Tab>Race</Tab>
+                <Tab>Class</Tab>
+                <Tab>Abilities</Tab>
+            </TabList>
+            <TabPanels>
+                <TabPanel>
+                    <Container>
+                        <Box background={"blackAlpha.600"} m={[2, 3]} p={[0.5, 1]} borderRadius='lg' shadow={"xl"}>
+                            <Flex m={[2, 3]} gap={5} flexDirection={"column"}>
+                                <FormControl>
+                                    <FormLabel>Name</FormLabel>
+                                    <Input placeholder=' '/>
+                                </FormControl>
+                                <FormControl>
+                                    <FormLabel>Class</FormLabel>
+                                    <Select placeholder=' '>
+                                        <option value='fighter'>Fighter</option>
+                                        <option value='cleric'>Cleric</option>
+                                    </Select>
+                                </FormControl>
+                                <FormControl>
+                                    <FormLabel>Race</FormLabel>
+                                    <Select placeholder=' '>
+                                        <option value='human'>Human</option>
+                                        <option value='dwarf'>Dwarf</option>
+                                    </Select>
+                                </FormControl>
+                                <Abilities/>
+                            </Flex>
+                        </Box>
+                    </Container>
+                </TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+            </TabPanels>
+        </Tabs>)
+
 }
 
 export default App
