4fa243a5afefe336fd46e3ad85772af99fbeb92d
francozanini <francozanini1995@gmail.com>
2022-06-01 22:13:09 -0300

    AbilitiesRow buttons padding

--- armory-web-client/src/App.tsx
+++ armory-web-client/src/App.tsx
@@ -71,7 +71,7 @@ function AbilityRow({ability}: { ability: Ability }) {
         <Td>{ability.name}</Td>
         <Td textAlign={"center"}>{score}</Td>
         <Td textAlign={"center"}>{ability.modifier()}</Td>
-        <Td>
+        <Td paddingX={1}>
             <Button onClick={() => setScore(ability.decreaseScore())} marginRight={1}>-</Button>
             <Button onClick={() => setScore(ability.increaseScore())}>+</Button>
         </Td>
