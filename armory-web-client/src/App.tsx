d80dc06461ea1d59d8fba1abddbd4dddefd517f0
francozanini <francozanini1995@gmail.com>
2022-06-02 21:17:04 -0300

    (refactor) extracted BlackBox component

--- armory-web-client/src/App.tsx
+++ armory-web-client/src/App.tsx
@@ -1,21 +1,9 @@
-import {
-    Box,
-    Container,
-    Flex,
-    FormControl,
-    FormLabel,
-    Input,
-    Select,
-    Tab,
-    TabList,
-    TabPanel,
-    TabPanels,
-    Tabs
-} from '@chakra-ui/react'
+import {FormControl, FormLabel, Input, Select, Tab, TabList, TabPanel, TabPanels, Tabs} from '@chakra-ui/react'
 
 import './App.css'
 import {Abilities} from "./Abilities";
-
+import React from "react";
+import {BlackBox} from "./BlackBox";
 
 function App() {
     return (
@@ -28,35 +16,40 @@ function App() {
             </TabList>
             <TabPanels>
                 <TabPanel>
-                    <Container>
-                        <Box background={"blackAlpha.600"} m={[2, 3]} p={[0.5, 1]} borderRadius='lg' shadow={"xl"}>
-                            <Flex m={[2, 3]} gap={5} flexDirection={"column"}>
-                                <FormControl>
-                                    <FormLabel>Name</FormLabel>
-                                    <Input placeholder=' '/>
-                                </FormControl>
-                                <FormControl>
-                                    <FormLabel>Class</FormLabel>
-                                    <Select placeholder=' '>
-                                        <option value='fighter'>Fighter</option>
-                                        <option value='cleric'>Cleric</option>
-                                    </Select>
-                                </FormControl>
-                                <FormControl>
-                                    <FormLabel>Race</FormLabel>
-                                    <Select placeholder=' '>
-                                        <option value='human'>Human</option>
-                                        <option value='dwarf'>Dwarf</option>
-                                    </Select>
-                                </FormControl>
-                                <Abilities/>
-                            </Flex>
-                        </Box>
-                    </Container>
+                    <BlackBox>
+                        <FormControl>
+                            <FormLabel>Name</FormLabel>
+                            <Input placeholder=' '/>
+                        </FormControl>
+                    </BlackBox>
                 </TabPanel>
-                <TabPanel></TabPanel>
-                <TabPanel></TabPanel>
-                <TabPanel></TabPanel>
+                <TabPanel>
+                    <BlackBox>
+                        <FormControl>
+                            <FormLabel>Race</FormLabel>
+                            <Select placeholder=' '>
+                                <option value='human'>Human</option>
+                                <option value='dwarf'>Dwarf</option>
+                            </Select>
+                        </FormControl>
+                    </BlackBox>
+                </TabPanel>
+                <TabPanel>
+                    <BlackBox>
+                        <FormControl>
+                            <FormLabel>Class</FormLabel>
+                            <Select placeholder=' '>
+                                <option value='fighter'>Fighter</option>
+                                <option value='cleric'>Cleric</option>
+                            </Select>
+                        </FormControl>
+                    </BlackBox>
+                </TabPanel>
+                <TabPanel>
+                    <BlackBox>
+                        <Abilities/>
+                    </BlackBox>
+                </TabPanel>
             </TabPanels>
         </Tabs>)
 
