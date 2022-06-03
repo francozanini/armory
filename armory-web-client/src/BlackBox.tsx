c03efab5f955020621c577d5f400713334331e76
francozanini <francozanini1995@gmail.com>
2022-06-02 22:20:21 -0300

    (refactor) const instead of function

--- armory-web-client/src/BlackBox.tsx
+++ armory-web-client/src/BlackBox.tsx
@@ -1,12 +1,10 @@
 import {Box, Flex} from "@chakra-ui/react";
 import React from "react";
 
-export function BlackBox({children}: { children: JSX.Element }) {
-    return (
-        <Box background={"blackAlpha.600"} m={[2, 3]} p={[0.5, 1]} borderRadius='lg' shadow={"xl"}>
-            <Flex m={[2, 3]} gap={5} flexDirection={"column"}>
-                {children}
-            </Flex>
-        </Box>
-    )
-}
+export const BlackBox = ({children}: { children: JSX.Element }) => (
+    <Box background={"blackAlpha.600"} m={[2, 3]} p={[0.5, 1]} borderRadius='lg' shadow={"xl"}>
+        <Flex m={[2, 3]} gap={5} flexDirection={"column"}>
+            {children}
+        </Flex>
+    </Box>
+)
