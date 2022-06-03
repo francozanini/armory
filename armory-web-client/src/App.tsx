20487a27ce66ebbaefdc171dfcae214d20082bde
francozanini <francozanini1995@gmail.com>
2022-06-02 21:18:37 -0300

    (refactor) extracted create character form

--- armory-web-client/src/App.tsx
+++ armory-web-client/src/App.tsx
@@ -1,57 +1,10 @@
-import {FormControl, FormLabel, Input, Select, Tab, TabList, TabPanel, TabPanels, Tabs} from '@chakra-ui/react'
-
 import './App.css'
-import {Abilities} from "./Abilities";
 import React from "react";
-import {BlackBox} from "./BlackBox";
+import {CreateCharacterForm} from "./CreateCharacterForm";
 
 function App() {
     return (
-        <Tabs>
-            <TabList>
-                <Tab>Details</Tab>
-                <Tab>Race</Tab>
-                <Tab>Class</Tab>
-                <Tab>Abilities</Tab>
-            </TabList>
-            <TabPanels>
-                <TabPanel>
-                    <BlackBox>
-                        <FormControl>
-                            <FormLabel>Name</FormLabel>
-                            <Input placeholder=' '/>
-                        </FormControl>
-                    </BlackBox>
-                </TabPanel>
-                <TabPanel>
-                    <BlackBox>
-                        <FormControl>
-                            <FormLabel>Race</FormLabel>
-                            <Select placeholder=' '>
-                                <option value='human'>Human</option>
-                                <option value='dwarf'>Dwarf</option>
-                            </Select>
-                        </FormControl>
-                    </BlackBox>
-                </TabPanel>
-                <TabPanel>
-                    <BlackBox>
-                        <FormControl>
-                            <FormLabel>Class</FormLabel>
-                            <Select placeholder=' '>
-                                <option value='fighter'>Fighter</option>
-                                <option value='cleric'>Cleric</option>
-                            </Select>
-                        </FormControl>
-                    </BlackBox>
-                </TabPanel>
-                <TabPanel>
-                    <BlackBox>
-                        <Abilities/>
-                    </BlackBox>
-                </TabPanel>
-            </TabPanels>
-        </Tabs>)
+        <CreateCharacterForm/>)
 
 }
 
