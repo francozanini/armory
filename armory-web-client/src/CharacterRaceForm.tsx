fdd22cf904fcd750b9564f64d459a1e52c6d51c3
francozanini <francozanini1995@gmail.com>
2022-06-04 12:43:14 -0300

    RaceList

--- armory-web-client/src/CharacterRaceForm.tsx
+++ armory-web-client/src/CharacterRaceForm.tsx
@@ -1,17 +1,29 @@
-import {FormControl, FormLabel, Select} from "@chakra-ui/react";
 import React from "react";
-import {withBlackBox} from "./WithBlackBox";
+import {Box, Center, Flex, Image} from "@chakra-ui/react";
 
 function CharacterRaceForm() {
+
+
     return (
-        <FormControl>
-            <FormLabel>Race</FormLabel>
-            <Select placeholder=" ">
-                <option value="human">Human</option>
-                <option value="dwarf">Dwarf</option>
-            </Select>
-        </FormControl>
+        <Center flexWrap={"wrap"}>
+            <Box background={"blackAlpha.600"}
+                 p={[0.5]}
+                 borderRadius='lg'
+                 flexBasis={['xl', "3xl", "4xl"]}
+            >
+                <Flex flexDirection={"row"} ml={3} alignItems={'center'}>
+                    <Image boxSize='50px'
+                           src='https://www.dndbeyond.com/avatars/9/386/636327460764467148.jpeg?width=1000&height=1000&fit=bounds&quality=95&auto=webp'
+                           alt="race-image"></Image>
+                    <Flex m={[2, 3]} gap={1} flexDirection={"column"}>
+                        <strong>Human</strong>
+                        <em>Basic Rules / Player's Handbook</em>
+                    </Flex>
+                </Flex>
+            </Box>
+        </Center>
+
     )
 }
 
-export default withBlackBox(CharacterRaceForm)
+export default CharacterRaceForm
