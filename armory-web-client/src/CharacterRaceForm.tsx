0a13ad194026e76e927a0143fbe42d067db9f46a
francozanini <francozanini1995@gmail.com>
2022-06-04 16:16:31 -0300

    allow style properties in Race Card

--- armory-web-client/src/CharacterRaceForm.tsx
+++ armory-web-client/src/CharacterRaceForm.tsx
@@ -1,26 +1,46 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Box, Center, Flex, Image} from "@chakra-ui/react";
 
-function CharacterRaceForm() {
+function RaceCard({race, styles}: { race: Race, styles?: React.CSSProperties }) {
+    const {raceName, avatarUrl, sourceBook} = race;
 
+    return <Box background={"blackAlpha.600"}
+                p={[0.5]}
+                borderRadius="lg"
+                flexBasis={["xl", "3xl", "4xl"]}
+                style={styles}
+    >
+        <Flex flexDirection={"row"} ml={3} alignItems={"center"}>
+            <Image boxSize="50px"
+                   src={avatarUrl}
+                   alt="race-image"></Image>
+            <Flex m={[2, 3]} gap={1} flexDirection={"column"}>
+                <strong>{raceName}</strong>
+                <em>{sourceBook}</em>
+            </Flex>
+        </Flex>
+    </Box>;
+}
+
+class Race {
+    constructor(
+        public readonly avatarUrl: string,
+        public readonly raceName: string,
+        public readonly sourceBook: string) {
+    }
+}
+
+function CharacterRaceForm() {
+    const [races, setRaces] = useState<Race[]>([]);
+    useEffect(() => {
+        fetch("http://localhost:8080/races")
+            .then(resp => resp.json())
+            .then(setRaces);
+    }, [])
 
     return (
         <Center flexWrap={"wrap"}>
-            <Box background={"blackAlpha.600"}
-                 p={[0.5]}
-                 borderRadius='lg'
-                 flexBasis={['xl', "3xl", "4xl"]}
-            >
-                <Flex flexDirection={"row"} ml={3} alignItems={'center'}>
-                    <Image boxSize='50px'
-                           src='https://www.dndbeyond.com/avatars/9/386/636327460764467148.jpeg?width=1000&height=1000&fit=bounds&quality=95&auto=webp'
-                           alt="race-image"></Image>
-                    <Flex m={[2, 3]} gap={1} flexDirection={"column"}>
-                        <strong>Human</strong>
-                        <em>Basic Rules / Player's Handbook</em>
-                    </Flex>
-                </Flex>
-            </Box>
+            {races.map(race => <RaceCard race={race} styles={{marginBottom: "8px"}}/>)}
         </Center>
 
     )
