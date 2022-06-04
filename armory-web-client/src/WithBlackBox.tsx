9c3bb6412318c7f338b7f506285bd8f54fce8489
francozanini <francozanini1995@gmail.com>
2022-06-03 21:43:57 -0300

    (refactor) allow props in hoc

--- armory-web-client/src/WithBlackBox.tsx
+++ armory-web-client/src/WithBlackBox.tsx
@@ -1,7 +1,7 @@
 import {Box, Flex} from "@chakra-ui/react";
 import React from "react";
 
-export const withBlackBox = (Component: () => JSX.Element) => {
+export const withBlackBox = (Component: React.FC) => {
     return (props: any) => (
         <Box background={"blackAlpha.600"} m={[2, 3]} p={[0.5, 1]} borderRadius='lg' shadow={"xl"}>
             <Flex m={[2, 3]} gap={5} flexDirection={"column"}>
