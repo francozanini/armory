432702a6ec46f6eccef38236bdc5ff14fff5a59e
francozanini <francozanini1995@gmail.com>
2022-06-04 16:13:38 -0300

    (chore) allow cors request from localhost

--- armory-server/src/main/kotlin/io/fz/armory/adapter/in/web/config/WebFluxConfig.kt
+++ armory-server/src/main/kotlin/io/fz/armory/adapter/in/web/config/WebFluxConfig.kt
@@ -1,10 +1,20 @@
 package io.fz.armory.adapter.`in`.web.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.config.CorsRegistry
 import org.springframework.web.reactive.config.EnableWebFlux
+import org.springframework.web.reactive.config.WebFluxConfigurer
+
 
 @Configuration
 @EnableWebFlux
-class WebFluxConfig
+class CorsGlobalConfiguration : WebFluxConfigurer {
+    override fun addCorsMappings(corsRegistry: CorsRegistry) {
+        corsRegistry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE")
+            .maxAge(3600)
+    }
+}
 
 
