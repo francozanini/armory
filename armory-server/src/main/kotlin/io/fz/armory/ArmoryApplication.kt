16dc87326e57a0c56b7eb6c67698ca40af83681f
francozanini <francozanini1995@gmail.com>
2022-06-04 23:28:41 -0300

    (refactor) named params

--- armory-server/src/main/kotlin/io/fz/armory/ArmoryApplication.kt
+++ armory-server/src/main/kotlin/io/fz/armory/ArmoryApplication.kt
@@ -23,16 +23,16 @@ class ArmoryRunner(val raceRepository: RaceDocumentRepository) : CommandLineRunn
 
         val races = listOf(
             RaceDocument(
-                null,
-                RaceName.HUMAN,
-                "https://www.dndbeyond.com/avatars/316/819/636620994416798583.jpeg?width=1000&height=1000&fit=bounds&quality=95&auto=webp",
-                "Player's Handbook"
+                id = null,
+                name = RaceName.HUMAN,
+                avatarUrl = "https://www.dndbeyond.com/avatars/316/819/636620994416798583.jpeg?width=1000&height=1000&fit=bounds&quality=95&auto=webp",
+                sourceBook = "Player's Handbook"
             ),
             RaceDocument(
-                null,
-                RaceName.DWARF,
-                "https://www.dndbeyond.com/avatars/316/806/636620993762055484.jpeg?width=1000&height=1000&fit=bounds&quality=95&auto=webp",
-                "Player's Handbook"
+                id = null,
+                name = RaceName.DWARF,
+                avatarUrl = "https://www.dndbeyond.com/avatars/316/806/636620993762055484.jpeg?width=1000&height=1000&fit=bounds&quality=95&auto=webp",
+                sourceBook = "Player's Handbook"
             ),
         )
 
