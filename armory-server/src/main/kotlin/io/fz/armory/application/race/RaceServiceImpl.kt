9ef019463bf9e1cbf7aaa6c9b296440db9aaba3d
francozanini <francozanini1995@gmail.com>
2022-06-06 20:55:06 -0300

    @Service annotation

--- armory-server/src/main/kotlin/io/fz/armory/application/race/RaceServiceImpl.kt
+++ armory-server/src/main/kotlin/io/fz/armory/application/race/RaceServiceImpl.kt
@@ -2,9 +2,11 @@ package io.fz.armory.application.race
 
 import io.fz.armory.application.race.port.RaceRepository
 import io.fz.armory.domain.Race
+import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
+@Service
 class RaceServiceImpl(private val raceRepository: RaceRepository) : RaceService {
     override fun findById(id: String): Mono<Race> {
         return Mono.justOrEmpty(id)
